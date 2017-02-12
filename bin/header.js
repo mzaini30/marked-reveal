82ed68805c624c300950a24f396ee19871a4398f
Muhammad Zaini <muhzaini30@gmail.com>
2017-02-12 14:59:48 +0800

    Update header.js

---
+++ bin/header.js
@@ -0,0 +1,23 @@
+document.writeln(" \
+<!DOCTYPE HTML>\
+<html>\
+<head>\
+    <title>presentasi</title>\
+    <meta charset='utf-8'>\
+    <meta name='viewport' content='width=device-width, initial-scale=1'>\
+    <link rel='stylesheet' href='reveal/css/reveal.css'/>\
+    <link rel='stylesheet' href='reveal/css/theme/league.css'/>\
+    <style>\
+        pre code {\
+            background: #fff;\
+            color: #000;\
+        }\
+        textarea.markdown {\
+            display: none;\
+        }\
+    </style>\
+</head>\
+<body>\
+")
+
+// theme: beige, black, blood, league, moon, night, serif, simple, sky, solarized, white
